0f90cb93487018e3fe6eecb98167124bd16cd4c0
Johan Singainy-Moutiapin <johan.singainy-moutiapin@opendatasoft.com>
2018-05-18 14:55:28 +0200

    [ch13169] add conf for pre-prod and official-prod

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,11 +14,6 @@ var JS_FILES = [
 
 module.exports = function(grunt) {
 
-    // Condition to switch resource file (js, css) in dev or prod
-    var distPath;
-    if (process.env.TRAVIS) distPath = 'https://static.opendatasoft.com/';
-    else distPath = '../dist/';
-
     // Project configuration.
     grunt.initConfig({
         uglify: {
@@ -122,11 +117,11 @@ module.exports = function(grunt) {
                     'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular.js',
                     'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-animate.js',
                     'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.4/angular-sanitize.js',
-                    distPath + 'ods-widgets.min.js',
+                    '../dist/ods-widgets.min.js',
                     '../docs-load-css.js'
                 ],
                 styles: [
-                    distPath + 'ods-widgets.min.css',
+                    '../dist/ods-widgets.min.css',
                     'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
                 ],
                 template: 'src/templates/index.tmpl',
